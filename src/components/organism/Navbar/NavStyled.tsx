0c32d7cb547d525a7bff56578c7f7ffb93b1c5cf
E127214 <kemal.taufik@aia.com>
2023-03-29 12:20:29 +0800

    creting navbar basic

--- src/components/organism/Navbar/NavStyled.tsx
+++ src/components/organism/Navbar/NavStyled.tsx
@@ -1,29 +1,50 @@
 import styled from "styled-components";
 import Link from "next/link";
-
 export const Nav = styled.nav`
   height: 70px;
   display: flex;
   justify-content: space-between;
   align-items: center;
-  /* background-color: red; */
-  box-shadow: 0px 2px 30px 0px rgba(0, 0, 0, 0.15);
+  background-color: white;
+  /* box-shadow: 0px 0px 0px 1px #eaeaea; */
+  border-style: solid;
+  border-width: 0 0 1px 0;
+  border-bottom-color: #eaeaea;
+  flex-wrap: wrap;
+  position: relative;
 `;
 
-export const LogoContainer = styled.div`
-  padding-left: 1.5rem;
+export const LogoContainer = styled(Link)`
   display: flex;
   align-items: center;
+  margin-left: 2rem;
 `;
 
 export const NavLinkContainer = styled.div`
-  margin: 1rem;
+  margin-right: 1rem;
+
+  @media (max-width: 768px) {
+    display: flex;
+    overflow: hidden;
+    flex-direction: column;
+    width: 100%;
+    align-items: left;
+    background-color: #fff;
+    margin: 0;
+    &.close {
+      position: absolute;
+      /* background-color: black; */
+      transition: all 0.5s ease-out;
+      /* top: -100%; */
+      transform: translateY(-100%);
+      z-index: -999;
+    }
+  }
 `;
 
 export const NavLinkStyled = styled(Link)`
   padding: 1rem;
   color: #003f59;
-
   &:hover {
     color: #000608;
   }
@@ -37,5 +58,87 @@ export const SiteTitle = styled.div`
   margin-left: 0.8rem;
   h1 {
     font-weight: 500;
+  }
+`;
+
+export const Hamburger = styled.div`
+  display: none;
+  flex-direction: column;
+  width: 25px;
+  height: 15px;
+  /* background-color: black; */
+  position: relative;
+  cursor: pointer;
+  -webkit-transform: rotate(0deg);
+  -moz-transform: rotate(0deg);
+  -o-transform: rotate(0deg);
+  transform: rotate(0deg);
+  -webkit-transition: 0.5s ease-in-out;
+  -moz-transition: 0.5s ease-in-out;
+  -o-transition: 0.5s ease-in-out;
+  transition: 0.5s ease-in-out;
+  cursor: pointer;
+
+  span {
+    display: block;
+    position: absolute;
+    height: 2px;
+    width: 100%;
+    background-color: #003f59;
+    margin-bottom: 4px;
+    border-radius: 5px;
+    left: 0;
+    -webkit-transform: rotate(0deg);
+    -moz-transform: rotate(0deg);
+    -o-transform: rotate(0deg);
+    transform: rotate(0deg);
+    -webkit-transition: 0.25s ease-in-out;
+    -moz-transition: 0.25s ease-in-out;
+    -o-transition: 0.25s ease-in-out;
+    transition: 0.25s ease-in-out;
+  }
+
+  span:nth-child(1) {
+    top: 0px;
+  }
+
+  span:nth-child(2),
+  span:nth-child(3) {
+    top: 7px;
+  }
+
+  span:nth-child(4) {
+    top: 14px;
+  }
+
+  &.open span:nth-child(1) {
+    top: 7px;
+    width: 0%;
+    left: 50%;
+  }
+
+  &.open span:nth-child(2) {
+    -webkit-transform: rotate(45deg);
+    -moz-transform: rotate(45deg);
+    -o-transform: rotate(45deg);
+    transform: rotate(45deg);
+  }
+
+  &.open span:nth-child(3) {
+    -webkit-transform: rotate(-45deg);
+    -moz-transform: rotate(-45deg);
+    -o-transform: rotate(-45deg);
+    transform: rotate(-45deg);
+  }
+
+  &.open span:nth-child(4) {
+    top: 7px;
+    width: 0%;
+    left: 50%;
+  }
+
+  @media (max-width: 768px) {
+    margin-right: 2rem;
+    display: flex;
   }
 `;
--- src/components/organism/Navbar/index.tsx
+++ src/components/organism/Navbar/index.tsx
@@ -1,22 +1,42 @@
 import Image from "next/image";
-import { LogoContainer, Nav, NavLinkContainer, SiteTitle } from "./NavStyled";
+import {
+  Hamburger,
+  LogoContainer,
+  Nav,
+  NavLinkContainer,
+  SiteTitle,
+} from "./NavStyled";
 import LogoImage from "../../../../public/images/logo_lifeplanjournal.png";
 import NavLink from "./NavLink";
 import { useRouter } from "next/router";
+import React, { useState } from "react";
 
 const Index = () => {
+  const [isOpen, setIsOpen] = useState(false);
   const router = useRouter();
   const currentRoute = router.pathname;
 
   return (
     <Nav>
-      <LogoContainer>
+      <LogoContainer href="/">
         <Image src={LogoImage} alt="logo" width={30}></Image>
         <SiteTitle>
           <h1>Life Plan Journal</h1>
         </SiteTitle>
       </LogoContainer>
-      <NavLinkContainer>
+      <Hamburger
+        className={isOpen ? "open" : ""}
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </Hamburger>
+      <NavLinkContainer
+        className={isOpen ? "" : "close"}
+        onClick={() => setIsOpen(!isOpen)}
+      >
         <NavLink href="/" active={currentRoute === "/" ? "active" : ""}>
           Home
         </NavLink>
