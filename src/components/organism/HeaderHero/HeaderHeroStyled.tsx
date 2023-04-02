159b36339aff8dc2c51f24d5bfe378bb85fe1895
kemaltf <kemaltaufikf@gmail.com>
2023-04-03 00:27:07 +0700

    fixing hero styled

---
+++ src/components/organism/HeaderHero/HeaderHeroStyled.tsx
@@ -0,0 +1,7 @@
+import { ContainerBg } from "@/styles/SharedStyles";
+import styled from "styled-components";
+
+export const ContainerBgStyled = styled(ContainerBg)`
+  width: 100%;
+  height: 50vh;
+`;
