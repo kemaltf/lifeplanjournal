5793086267f827dd1381e20ef9feb49ac54097f2
kemal <kemaltaufikf@gmail.com>
2023-04-03 16:09:36 +0800

    refactoring code

---
+++ src/components/organism/ProductCards/ProductCardStyled.tsx
@@ -0,0 +1,8 @@
+import { Container, ContainerPad } from "@/styles/SharedStyles";
+import styled from "styled-components";
+
+export const ProductCardsContainer = styled(ContainerPad)`
+  flex-wrap: wrap;
+  justify-content: flex-start;
+  padding: 0 200px;
+`;
