fb79afb98ede1f856c5e030b1dc9f758d2b9b34d
kemal <kemaltaufikf@gmail.com>
2023-05-12 10:40:16 +0800

    Hero refactor imageLen

--- src/components/molecules/Hero/index.tsx
+++ src/components/molecules/Hero/index.tsx
@@ -9,7 +9,14 @@ export default function index({ images }: IndexProps) {
   const [currentImage, setCurrentImage] = useState(1);
   const [durationMs, setDurationMs] = useState<number>(400); // container width
   return (
-    <Carousel images={images} currentImage={currentImage} setCurrentImage={setCurrentImage} setDurationMs={setDurationMs} loop={true} durationMs={durationMs}>
+    <Carousel
+      imagesLen={images.length}
+      currentImage={currentImage}
+      setCurrentImage={setCurrentImage}
+      setDurationMs={setDurationMs}
+      loop={true}
+      durationMs={durationMs}
+    >
       <ImageCarousel image={images[images.length - 1]} key={-1}></ImageCarousel>
       {images.map((image: string, index) => (
         <ImageCarousel image={image} key={index}></ImageCarousel>
