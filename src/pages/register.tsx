e45ee4bf5f78c62186bd8eb80c816d2f7de91f56
kemaltf <kemaltaufikf@gmail.com>
2023-05-24 16:49:05 +0700

    create login and register page

---
+++ src/pages/register.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+type Props = {};
+
+import RegisterComponent from "@/components/organism/Register";
+const Register = (props: Props) => {
+  return <RegisterComponent></RegisterComponent>;
+};
+
+export default Register;
