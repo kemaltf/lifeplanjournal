a87705078e01e5d1e4c2b9bde9e680457825ae16
kemaltf <kemaltaufikf@gmail.com>
2023-05-28 19:25:26 +0700

    navbar context correct

--- src/components/organism/Navbar/index.tsx
+++ src/components/organism/Navbar/index.tsx
@@ -1,38 +1,36 @@
-import Image from "next/image";
-import { Hamburger, LogoContainer, Nav, NavLinkContainer, SiteTitle } from "./NavStyled";
-import LogoImage from "../../../../public/images/logo_lifeplanjournal.png";
-import NavLink from "./NavLink";
+import { Nav, NavLinkContainer, NavLinkStyled, Overlay } from "./NavStyled";
 import { useRouter } from "next/router";
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import SiteLogo from "./SiteLogo";
+import SiteImage from "../../../../public/images/logo_lifeplanjournal.png";
+import HamburgerComponent from "./Hamburger";
+import LoginNavComponent from "./LoginNav";
+import OverlayStatusContext from "@/context/OverlayStatusContext";
 
 const Index = () => {
   const [isOpen, setIsOpen] = useState(false);
   const router = useRouter();
   const currentRoute = router.pathname;
-
+  const overlayStatus = useContext(OverlayStatusContext);
+  console.log(overlayStatus);
   return (
-    <Nav>
-      <LogoContainer href="/">
-        <Image src={LogoImage} alt="logo" width={27}></Image>
-        <SiteTitle>
-          <h1>Tes</h1>
-        </SiteTitle>
-      </LogoContainer>
-      <Hamburger className={isOpen ? "open" : ""} onClick={() => setIsOpen(!isOpen)}>
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-      </Hamburger>
-      <NavLinkContainer className={isOpen ? "" : "close"} onClick={() => setIsOpen(!isOpen)}>
-        <NavLink href="/" active={currentRoute === "/" ? "active" : ""}>
-          Home
-        </NavLink>
-        <NavLink href="/products" active={currentRoute === "/products" ? "active" : ""}>
-          Products
-        </NavLink>
-      </NavLinkContainer>
-    </Nav>
+    <>
+      {/* add context */}
+      {overlayStatus.overlayStatus && <Overlay />}
+      <Nav>
+        <SiteLogo alt="Life Plan Journal" image={SiteImage} href="/" titleImage="Tes"></SiteLogo>
+        <HamburgerComponent className={isOpen ? "open" : ""} onClick={() => setIsOpen(!isOpen)}></HamburgerComponent>
+        <NavLinkContainer className={isOpen ? "" : "close"} onClick={() => setIsOpen(!isOpen)}>
+          <NavLinkStyled href="/" className={currentRoute === "/" ? "active" : ""}>
+            Home
+          </NavLinkStyled>
+          <NavLinkStyled href="/products" className={currentRoute === "/products" ? "active" : ""}>
+            Products
+          </NavLinkStyled>
+          <LoginNavComponent></LoginNavComponent>
+        </NavLinkContainer>
+      </Nav>
+    </>
   );
 };
 
